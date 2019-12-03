7927a3424537bb4caf5019f5e3acee4c688f30fe
Dalton098 <Dalton098@ymail.com>
2019-12-03 09:39:46 -0500

    Added skeleton code to all queries but might need to be altered

--- src/cs377_finalproject/sqlConnections.java
+++ src/cs377_finalproject/sqlConnections.java
@@ -9,83 +9,428 @@ import java.sql.*;
 public class sqlConnections {
 
     private static Connection conn;
-    private static Statement stmt;
+//    private static Statement stmt;
+    private static PreparedStatement pstmt;
 
-    public static void insertCar() {
-//        regnum, make, model, year, sticker price
+    public static void insertCar() throws SQLException {
+
+        //        regnum, make, model, year, sticker price
+        try {
+            establishConnectionToDatabase();
+            String query = "";
+            pstmt = conn.prepareStatement(query);
+
+//            insert query shit here (use prepared statement)
+        } catch (Exception e) {
+
+            System.out.println("ERROR: Could not insert car");
+
+        } finally {
+            if (conn != null) {
+                conn.close();
+            }
+
+            if (pstmt != null) {
+                pstmt.close();
+            }
+
+        }
+
     }
 
-    public static void insertSale() {
+    public static void insertSale() throws SQLException {
 //        needs reg num, sale price, employee ssn, date
 
+        try {
+            establishConnectionToDatabase();
+            String query = "";
+            pstmt = conn.prepareStatement(query);
+
+//            insert query shit here (use prepared statement)
+        } catch (Exception e) {
+
+            System.out.println("ERROR: Could not insert sale");
+
+        } finally {
+            if (conn != null) {
+                conn.close();
+            }
+
+            if (pstmt != null) {
+                pstmt.close();
+            }
+
+        }
+
     }
 
-    public static void insertDepartment() {
+    public static void insertDepartment() throws SQLException {
 //        needs department name
 
+        try {
+            establishConnectionToDatabase();
+            String query = "";
+            pstmt = conn.prepareStatement(query);
+
+//            insert query shit here (use prepared statement)
+        } catch (SQLException e) {
+
+            System.out.println("ERROR: Could not insert department");
+
+        } finally {
+            stmt = conn.createStatement();
+            if (conn != null) {
+                conn.close();
+            }
+
+            if (pstmt != null) {
+                pstmt.close();
+            }
+
+        }
+
     }
 
-    public static void updateSale() {
+    public static void updateSale() throws SQLException {
 //        changes sale price and date
 //        could overload to take price, date, or price and date
+
+        try {
+            establishConnectionToDatabase();
+            String query = "";
+            pstmt = conn.prepareStatement(query);
+
+//            insert query shit here (use prepared statement)
+        } catch (SQLException e) {
+
+            System.out.println("ERROR: Could not update sale");
+
+        } finally {
+            if (conn != null) {
+                conn.close();
+            }
+
+            if (pstmt != null) {
+                pstmt.close();
+            }
+
+        }
     }
 
-    public static void updateEmployeeInfo() {
+    public static void updateEmployeeInfo() throws SQLException {
 //        vague as fuck not sure what to do with this
 //        could take in array with the names and array with values or something
+
+        try {
+            establishConnectionToDatabase();
+            String query = "";
+            pstmt = conn.prepareStatement(query);
+
+//            insert query shit here (use prepared statement)
+        } catch (SQLException e) {
+
+            System.out.println("ERROR: Could not update employee info");
+
+        } finally {
+            if (conn != null) {
+                conn.close();
+            }
+
+            if (pstmt != null) {
+                pstmt.close();
+            }
+
+        }
     }
 
-    public static void updateCarAvailability() {
+    public static void updateCarAvailability() throws SQLException {
 //        only changes availability bit value
+
+        try {
+            establishConnectionToDatabase();
+            String query = "";
+            pstmt = conn.prepareStatement(query);
+
+//            insert query shit here (use prepared statement)
+        } catch (SQLException e) {
+
+            System.out.println("ERROR: Could not update availability");
+
+        } finally {
+            if (conn != null) {
+                conn.close();
+            }
+
+            if (pstmt != null) {
+                pstmt.close();
+            }
+
+        }
     }
 
-    public static void updateSalary() {
+    public static void updateSalary() throws SQLException {
 //    only changes salary    
+        try {
+            establishConnectionToDatabase();
+            String query = "";
+            pstmt = conn.prepareStatement(query);
+
+//            insert query shit here (use prepared statement)
+        } catch (SQLException e) {
+
+            System.out.println("ERROR: Could not update salary");
+
+        } finally {
+            if (conn != null) {
+                conn.close();
+            }
+
+            if (pstmt != null) {
+                pstmt.close();
+            }
+
+        }
     }
 
-    public static void updateDepartment() {
+    public static void updateDepartment() throws SQLException {
 //        only changes name
+
+        try {
+            establishConnectionToDatabase();
+            String query = "";
+            pstmt = conn.prepareStatement(query);
+
+//            insert query shit here (use prepared statement)
+        } catch (SQLException e) {
+
+            System.out.println("ERROR: Could not update department");
+
+        } finally {
+            if (conn != null) {
+                conn.close();
+            }
+
+            if (pstmt != null) {
+                pstmt.close();
+            }
+
+        }
     }
 
-    public static void deleteEmployee() {
+    public static void deleteEmployee() throws SQLException {
 //        i think this will automatically handle setting null to people who use the deleteed employee as a manager
+        try {
+            establishConnectionToDatabase();
+            String query = "";
+            pstmt = conn.prepareStatement(query);
+
+//            insert query shit here (use prepared statement)
+        } catch (SQLException e) {
+
+            System.out.println("ERROR: Could not delete Employee");
+
+        } finally {
+            if (conn != null) {
+                conn.close();
+            }
+
+            if (pstmt != null) {
+                pstmt.close();
+            }
+
+        }
     }
 
 //    public static void deleteCar() {
 ////        might not need this because then will have to delete the sale
 //    }
-    public static void deleteDepartment() {
+    public static void deleteDepartment() throws SQLException {
 //        should automatically set employees in this department to have a null deptid
+
+        try {
+            establishConnectionToDatabase();
+            String query = "";
+            pstmt = conn.prepareStatement(query);
+
+//            insert query shit here (use prepared statement)
+        } catch (SQLException e) {
+
+            System.out.println("ERROR: Could not delete department");
+
+        } finally {
+            if (conn != null) {
+                conn.close();
+            }
+
+            if (pstmt != null) {
+                pstmt.close();
+            }
+
+        }
     }
 
-    public static String selectSpecificCar() {
+    public static String selectSpecificCar() throws SQLException {
 //        takes reg num
+
+        try {
+            establishConnectionToDatabase();
+            String query = "";
+            pstmt = conn.prepareStatement(query);
+
+//            insert query shit here (use prepared statement)
+            return null;
+
+        } catch (SQLException e) {
+
+            System.out.println("ERROR: Could not search for the given car");
+
+        } finally {
+            if (conn != null) {
+                conn.close();
+            }
+
+            if (pstmt != null) {
+                pstmt.close();
+            }
+
+        }
         return null;
     }
 
-    public static String selectCars() {
+    public static String selectCars() throws SQLException {
 //        takes make, model, year and returns all cars of this type
+
+        try {
+            establishConnectionToDatabase();
+            String query = "";
+            pstmt = conn.prepareStatement(query);
+
+//            insert query shit here (use prepared statement)
+            return null;
+
+        } catch (SQLException e) {
+
+            System.out.println("ERROR: Could not search for the given cars");
+
+        } finally {
+            if (conn != null) {
+                conn.close();
+            }
+
+            if (pstmt != null) {
+                pstmt.close();
+            }
+
+        }
         return null;
     }
 
-    public static String selectSale() {
+    public static String selectSale() throws SQLException {
 //        use sale id
+        try {
+            establishConnectionToDatabase();
+            String query = "";
+            pstmt = conn.prepareStatement(query);
+
+//            insert query shit here (use prepared statement)
+            return null;
+
+        } catch (SQLException e) {
+
+            System.out.println("ERROR: Could not search for the given sale");
+
+        } finally {
+            if (conn != null) {
+                conn.close();
+            }
+
+            if (pstmt != null) {
+                pstmt.close();
+            }
+
+        }
         return null;
     }
 
-    public static String selectDepartment() {
+    public static String selectDepartment() throws SQLException {
 //        uses department id
+        try {
+            establishConnectionToDatabase();
+            String query = "";
+            pstmt = conn.prepareStatement(query);
+
+//            insert query shit here (use prepared statement)
+            return null;
+
+        } catch (SQLException e) {
+
+            System.out.println("ERROR: Could not search for the given department");
+
+        } finally {
+            if (conn != null) {
+                conn.close();
+            }
+
+            if (pstmt != null) {
+                pstmt.close();
+            }
+
+        }
         return null;
     }
 
-    public static String inventoryReport() {
+    public static String inventoryReport() throws SQLException {
 //        count up # of cars by make, model, year and are available
+        try {
+            establishConnectionToDatabase();
+            String query = "";
+            pstmt = conn.prepareStatement(query);
+
+//            insert query shit here (use prepared statement)
+            return null;
+
+        } catch (SQLException e) {
+
+            System.out.println("ERROR: Could not complete inventory report");
+
+        } finally {
+            if (conn != null) {
+                conn.close();
+            }
+
+            if (pstmt != null) {
+                pstmt.close();
+            }
+
+        }
         return null;
     }
 
-    public static String salesReport() {
+    public static String salesReport() throws SQLException {
 //        shows lowest sale, highest sale, avg sale for each make, model, and year
 //        most likely gonna be a hell a lot of joins between the tables but will reuse a lot of the inventory report query
+        try {
+            establishConnectionToDatabase();
+            String query = "";
+            pstmt = conn.prepareStatement(query);
+
+//            insert query shit here (use prepared statement)
+            return null;
+
+        } catch (SQLException e) {
+
+            System.out.println("ERROR: Could not complete sales report");
+
+        } finally {
+            if (conn != null) {
+                conn.close();
+            }
+
+            if (pstmt != null) {
+                pstmt.close();
+            }
+
+        }
         return null;
     }
 
